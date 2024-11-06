bc549603435f469d45f4080c35410200743952f5
MiguelDV <miguel.diav84@gmail.com>
2024-11-06 17:56:19 +0100

    Cambio de IMG a SVG en el Logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,282 +1,52 @@
-import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import Section from "../Section/Section";
-import logo from "../../assets/images/Logo Phi-Footer.png";
-import { Box, IconButton, Link } from "@mui/material";
+import "./Footer.css";
+import LogoIcon from "../Icons/LogoIcon";
 
 export default function Footer() {
   return (
     <Section>
-      <Box
-        component="footer"
-        sx={{
-          display: "grid",
-          gridTemplateColumns: { xs: "1fr 1fr", md: "1fr 2fr 1fr" },
-          alignItems: "center",
-          padding: "1rem",
-          width: "85vw",
-          margin: "0 auto",
-          boxSizing: "border-box",
-          gap: { xs: "1rem", md: "0" },
-        }}
-      >
+      <footer className="footer-container">
         {/* Columna 1: Logo */}
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-start",
-            cursor: "pointer",
-          }}
-          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        >
-          <img
-            src={logo}
-            alt="PHI Alliance Logo"
-            style={{
-              width: "100px",
-              height: "auto",
-            }}
-          />
-        </Box>
+        <LogoIcon />
 
-        {/* Columna 2: Enlaces (Ocultos en pantallas pequeñas) */}
-        <Box
-          sx={{
-            display: { xs: "none", md: "flex" },
-            justifyContent: "right",
-            gap: "2rem",
-            flexDirection: "row",
-          }}
-        >
-          <Box component="ul" sx={{ listStyle: "none", padding: 0 }}>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              Home
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              App
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              About
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              People
-            </Link>
-          </Box>
-          <Box component="ul" sx={{ listStyle: "none", padding: 0 }}>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              For school
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              Brain training
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              E-learning
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              Online modules
-            </Link>
-          </Box>
-          <Box component="ul" sx={{ listStyle: "none", padding: 0 }}>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              Media
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              Support Us
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                mb: 0.5,
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              Contact
-            </Link>
-            <Link
-              href="#"
-              component="li"
-              sx={{
-                color: "#686868",
-                textDecoration: "none",
-                cursor: "pointer",
-                transition: "color 0.3s",
-                "&:hover": { color: "#404040" },
-              }}
-            >
-              Places
-            </Link>
-          </Box>
-        </Box>
+        {/* Columna 2: Enlaces */}
+        <div className="footer-links">
+          <ul>
+            <li>Home</li>
+            <li>App</li>
+            <li>About</li>
+            <li>People</li>
+          </ul>
+          <ul>
+            <li>For school</li>
+            <li>Brain training</li>
+            <li>E-learning</li>
+            <li>Online modules</li>
+          </ul>
+          <ul>
+            <li>Media</li>
+            <li>Support Us</li>
+            <li>Contact</li>
+            <li>Places</li>
+          </ul>
+        </div>
 
         {/* Columna 3: Redes Sociales */}
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-            gap: "1rem",
-            position: "relative",
-            top: "-2.5vw",
-          }}
-        >
-          <IconButton
-            href="#"
-            sx={{
-              color: "#574ea0",
-              border: "1px solid #574ea0",
-              padding: "0.5rem",
-              borderRadius: "50%",
-              transition: "background-color 0.3s, color 0.3s",
-              alignSelf: "flex-start",
-              "&:hover": { backgroundColor: "#574ea0", color: "#ffffff" },
-            }}
-          >
+        <div className="footer-socials">
+          <a href="#" className="social-icon">
             <InstagramIcon />
-          </IconButton>
-          <IconButton
-            href="#"
-            sx={{
-              color: "#574ea0",
-              border: "1px solid #574ea0",
-              padding: "0.5rem",
-              borderRadius: "50%",
-              transition: "background-color 0.3s, color 0.3s",
-              alignSelf: "flex-start",
-              "&:hover": { backgroundColor: "#574ea0", color: "#ffffff" },
-            }}
-          >
+          </a>
+          <a href="#" className="social-icon">
             <LinkedInIcon />
-          </IconButton>
-          <IconButton
-            href="#"
-            sx={{
-              color: "#574ea0",
-              border: "1px solid #574ea0",
-              padding: "0.5rem",
-              borderRadius: "50%",
-              transition: "background-color 0.3s, color 0.3s",
-              alignSelf: "flex-start",
-              "&:hover": { backgroundColor: "#574ea0", color: "#ffffff" },
-            }}
-          >
+          </a>
+          <a href="#" className="social-icon">
             <TwitterIcon />
-          </IconButton>
-        </Box>
-      </Box>
+          </a>
+        </div>
+      </footer>
     </Section>
   );
 }
--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -8,10 +8,16 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import { useState } from "react";
-import PhiAllianceIco from "../../../assets/phi-alliance.png";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import LogoIcon from "../../Icons/LogoIcon";
 
-const pages = ["Home", "Qué, cómo y por qué", "Comunidad", "Proyectos", "Síguenos"]; 
+const pages = [
+  "Home",
+  "Qué, cómo y por qué",
+  "Comunidad",
+  "Proyectos",
+  "Síguenos",
+];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -25,133 +31,124 @@ function ResponsiveAppBar() {
   };
 
   return (
-      <AppBar
-        position="static"
-        sx={{ backgroundColor: "white", width: "100%", padding: "1rem 8rem" }} 
-        elevation={0}
-      >
-        <Toolbar disableGutters>
-          <Box
-            component={"section"}
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "white", width: "100%", padding: "1rem 8rem" }}
+      elevation={0}
+    >
+      <Toolbar disableGutters>
+        <Box
+          component={"section"}
+          sx={{
+            display: "inline-block",
+          }}
+        >
+          <LogoIcon />
+        </Box>
+
+        {/* Contenedor del menú móvil */}
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "flex", md: "none" },
+            justifyContent: "end",
+            alignItems: "center",
+            color: "#5BC7C0",
+          }}
+        >
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
             sx={{
-              display: "inline-block",
+              border: "1px solid #5BC7C0",
             }}
           >
-            <img
-              src={PhiAllianceIco}
-              alt="Phi Alliance"
-              width="100px"
-              height="100px"
-              style={{
-                verticalAlign: "bottom",
-                borderRadius: "15px",
-              }}
-            />
-          </Box>
+            <MenuIcon />
+          </IconButton>
 
-          {/* Contenedor del menú móvil */}
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "flex", md: "none" },
-              justifyContent: "end",
-              alignItems: "center",
-              color: "#5BC7C0",
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
             }}
-          >
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-              sx={{
-                border: "1px solid #5BC7C0",
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          {/* Contenedor de botones para pantallas más grandes */}
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex" },
-              justifyContent: "end",
-              alignItems: "center",
-              gap: "1rem",
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
             }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{ display: { xs: "block", md: "none" } }}
           >
             {pages.map((page) => (
-              <Button
-                key={page}
-                variant="outlined"
-                sx={{
-                  color: "black",
-                  borderRadius: "25px",
-                  borderColor: "#CCCCCC",
-                  fontFamily: "Poppins, sans-serif",
-                  fontSize: "1.5rem",
-                  textTransform: "none",
-                  padding: ".2rem 2rem",
-                }}
-              >
-                {page}
-              </Button>
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+              </MenuItem>
             ))}
-          </Box>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex" },
-              justifyContent: "end",
-              alignItems: "center",
-            }}
-          >
+          </Menu>
+        </Box>
+
+        {/* Contenedor de botones para pantallas más grandes */}
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "end",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          {pages.map((page) => (
             <Button
+              key={page}
               variant="outlined"
               sx={{
-                color: "green",
+                color: "black",
                 borderRadius: "25px",
-                borderColor: "green",
+                borderColor: "#CCCCCC",
                 fontFamily: "Poppins, sans-serif",
-                textTransform: "none",
                 fontSize: "1.5rem",
-                gap: "5px",
-                height: "4rem",
+                textTransform: "none",
+                padding: ".2rem 2rem",
               }}
             >
-              Contactanos
-              <WhatsAppIcon />
+              {page}
             </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
+          ))}
+        </Box>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "end",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
+            sx={{
+              color: "green",
+              borderRadius: "25px",
+              borderColor: "green",
+              fontFamily: "Poppins, sans-serif",
+              textTransform: "none",
+              fontSize: "1.5rem",
+              gap: "5px",
+              height: "4rem",
+            }}
+          >
+            Contactanos
+            <WhatsAppIcon />
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 
