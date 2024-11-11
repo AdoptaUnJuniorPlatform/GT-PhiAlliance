99a8512b02580e9a5f7efc72bdf7dfedf3f45bf5
Miguel Díaz <miguel.museo@gmail.com>
2024-11-11 13:36:49 +0100

    Ajustes en el tamaños de tarjetas y posicionamiento

--- src/components/QueYParaQue/ContainerCards/Card/Card.jsx
+++ src/components/QueYParaQue/ContainerCards/Card/Card.jsx
@@ -1,6 +1,6 @@
 import styles from "./Card.module.css";
 
-export default function Card({ icon, title, description, color, size, handleSize }) {
+export default function Card({ icon, title, description, color, size, handleSize, fullDescripcion }) {
     const getClassName = () => {
         switch (color) {
             case "blue":
@@ -17,15 +17,22 @@ export default function Card({ icon, title, description, color, size, handleSize
 
     return size ? (
         <div className={getClassName()} onMouseEnter={handleSize}>
-            <div>{icon}</div>
-            <span>{title}</span>
+            <div className={styles.cardContent}>
+                <div>{icon}</div>
+                <span>{title}</span>
+            </div>
+
             <p>{description}</p>
         </div>
     ) : (
-        <div className={`${getClassName()} ${styles.cardMaxWidth}`} onMouseLeave={handleSize}>  
-            <div>{icon}</div>
-            <span>{title}</span>
-            <p>{description}</p>
+        <div className={`${getClassName()} ${styles.cardMaxWidth}`} onMouseLeave={handleSize}>
+            <div className={styles.cardMaxWidthDiv}>
+                <div className={styles.cardContent}>
+                    <div>{icon}</div>
+                    <span>{title}</span>
+                </div>
+                <p>{fullDescripcion}</p>
+            </div>
         </div>
     )
 }
