76609d197be7b9b2cf35963a9f555627194b3c21
MiguelDV <miguel.diav84@gmail.com>
2024-11-20 19:09:02 +0100

    Resolucion de conflictos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,64 @@
-import { Box } from "@mui/material";
+import { useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import FollowUs from "../components/FollowUs/FollowUs";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
 import Home from "../components/Home/Home";
 import Projects from "../components/Projects/Projects";
 import Team from "../components/Team/Team";
 import AboutUs from "../components/AboutUs/AboutUs";
-import Colaboradores from "../components/Colaboradores/Colaboradores"
+import Colaboradores from "../components/Colaboradores/Colaboradores";
 
 export default function HomePage() {
+  // Crear referencias para cada sección
+  const homeRef = useRef(null);
+  const aboutUsRef = useRef(null);
+  const projectsRef = useRef(null);
+  const comunidadRef = useRef(null);
+  const followUsRef = useRef(null);
+
+  // Obtener el estado de la navegación
+  const location = useLocation();
+
+  useEffect(() => {
+    // Comprobar si existe un estado y desplazarse a la sección correspondiente
+    if (location.state?.section) {
+      scrollToSection(location.state.section);
+    }
+  }, [location]);
+
+  const scrollToSection = (section) => {
+    // Desplazarse a la sección indicada
+    const sectionsMap = {
+      home: homeRef,
+      aboutUs: aboutUsRef,
+      projects: projectsRef,
+      comunidad: comunidadRef,
+      followUs: followUsRef,
+    };
+
+    const targetRef = sectionsMap[section];
+    if (targetRef?.current) {
+      targetRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <Box component="section" sx={{ padding: "2rem 4rem" }}>
-      <Header />
-      <Home />
-      <AboutUs />
-      <Projects />
-      <Team />
-      <Colaboradores/>
-      <FollowUs />
-      <Footer />
-    </Box>
+    <>
+      <div ref={homeRef}>
+        <Home />
+      </div>
+      <div ref={aboutUsRef}>
+        <AboutUs />
+      </div>
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
+      <Colaboradores />
+      <div ref={comunidadRef}>
+        <Team />
+      </div>
+      <div ref={followUsRef}>
+        <FollowUs />
+      </div>
+    </>
   );
 }
