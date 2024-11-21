c669c54d658d6c102e951d8e06e49a7e4b37cb5f
MiguelDV <miguel.diav84@gmail.com>
2024-11-21 18:16:35 +0100

    Se ha añadido la informacion en la seccion que como y porque

--- src/components/QueYParaQue/ContainerCards/ContainerCards.jsx
+++ src/components/QueYParaQue/ContainerCards/ContainerCards.jsx
@@ -9,7 +9,7 @@ import { aboutUs } from "../../../assets/data/AboutUs";
 export default function ContainerCards() {
   const [sizeBlue, setSizeBlue] = useState(true);
   const [sizePurple, setSizePurple] = useState(true);
-  const [sizeYellow, setSizeYellow] = useState(true);
+  const [sizeYellow, setSizeYellow] = useState(false);
 
   const handleSizeBlue = () => {
     setSizeBlue(!sizeBlue);
@@ -45,12 +45,25 @@ export default function ContainerCards() {
       <Card
         description={aboutUs.cards[1].subtitle}
         fullDescripcion={aboutUs.cards[1].content}
-        fullDescripcion1={<>
-          <p><strong>Aspiramos a:</strong></p>
-          <p><strong>Reducir desigualdades:</strong> {aboutUs.cards[1].content[1]}</p>
-          <p><strong>Acelerar la sostenibilidad: </strong>{aboutUs.cards[1].content[2]}</p>
-          <p><strong>Empoderar y conectar comunidades: </strong>{aboutUs.cards[1].content[3]}</p>
-        </>}
+        fullDescripcion1={
+          <>
+            <p>
+              <strong>Aspiramos a:</strong>
+            </p>
+            <p>
+              <strong>Reducir desigualdades:</strong>{" "}
+              {aboutUs.cards[1].content[1]}
+            </p>
+            <p>
+              <strong>Acelerar la sostenibilidad: </strong>
+              {aboutUs.cards[1].content[2]}
+            </p>
+            <p>
+              <strong>Empoderar y conectar comunidades: </strong>
+              {aboutUs.cards[1].content[3]}
+            </p>
+          </>
+        }
         icon={<EstrellasIcon />}
         title={aboutUs.cards[1].title}
         color="purple"
@@ -59,7 +72,8 @@ export default function ContainerCards() {
       />
       <Card
         description={aboutUs.cards[2].subtitle}
-        fullDescripcion={`${aboutUs.cards[2].content[0]} ${aboutUs.cards[2].content[1]}`}
+        fullDescripcion={`${aboutUs.cards[2].content[0]} `}
+        fullDescripcion1={<strong>{aboutUs.cards[2].content[1]}</strong>}
         icon={<IAIcon />}
         title={aboutUs.cards[2].title}
         color="yellow"
